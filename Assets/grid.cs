8af8344b7237ae9dd61ebaae07ef51c6814b5f50
Hugo Caroumbin | "RatonFuté <hcaroumbin@gaming.tech>
2023-02-22 15:43:30 +0100

    timer

--- Assets/grid.cs
+++ Assets/grid.cs
@@ -3,29 +3,76 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
+using static UnityEngine.UI.Image;
 
 public class grid : MonoBehaviour
 {
     [SerializeField] GameObject square;
     [SerializeField] GameObject outline;
-    int timeleft = 10;
-    Text timer;
+    [SerializeField] GameObject bomb;
+    [SerializeField] float timeleft;
+    [SerializeField] Text timerText;
+    int displayedTime;
+    Vector3 position;
+    public int countBomb;
     
     // Start is called before the first frame update
     void Start()
     {
-        for(int i =-4; i < 5; i++)
-        {
-            for(int j = -4;j < 5; j++)
-            {
-                Instantiate(outline, new Vector2(i, j), Quaternion.identity);
-                Instantiate(square, new Vector2(i, j), Quaternion.identity);
-            }
-        } 
+        
+        timerText.text = timeleft.ToString() ;
+        drawGrid();
+        drawBomb();
     }
 
     private void Update()
     {
+        if(timeleft > 0)
+        {
+            timeleft -= Time.deltaTime;
+            displayedTime = (int)timeleft;
+        }
+        if(timeleft < 0)
+        {
+            timeleft = 0;
+        }
+        timerText.text = "time left : " + displayedTime.ToString();
         
+    }
+    void drawGrid ()
+    {
+        for (int i = -4; i < 5; i++)
+        {
+            for (int j = -4; j < 5; j++)
+            {
+                Instantiate(outline, new Vector2(i, j), Quaternion.identity);
+                Instantiate(square, new Vector2(i, j), Quaternion.identity);
+            }
+        }
+    }
+    void drawBomb()
+    {
+
+        position = new Vector3(Random.Range(-4, 4), Random.Range(-4, 4), -2);
+        Instantiate(bomb, position, Quaternion.identity);
+        position = new Vector3(Random.Range(-4, 4), Random.Range(-4, 4), -2);
+        Instantiate(bomb, position, Quaternion.identity);
+        position = new Vector3(Random.Range(-4, 4), Random.Range(-4, 4), -2);
+        Instantiate(bomb, position, Quaternion.identity);
+        position = new Vector3(Random.Range(-4, 4), Random.Range(-4, 4), -2);
+        Instantiate(bomb, position, Quaternion.identity);
+
+    }
+    void addCount()
+    {
+        
+        Vector3 posTemp = position + new Vector3(1, 0, 0);
+        
+        //(position + new Vector3(1, -1, 0))
+        //(position + new Vector3(0, -1, 0))
+        //(position + new Vector3(-1, -1, 0))
+        //(position + new Vector3(-1, 0, 0))
+        //(position + new Vector3(0, 1, 0))
+            { countBomb += 1; }
     }
 }
