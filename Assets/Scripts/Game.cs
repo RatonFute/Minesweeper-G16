a92c8ba79275855e38f7acebaddf3718b94f3b3c
unknown <mleconte@gamingcampus.local>
2023-02-27 09:08:26 +0100

    flagthemall

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -115,7 +115,6 @@ public class Game : MonoBehaviour
             {
                 state[x, y].type = Cell.Type.Mine;
             }
-            
         }
     }
 
@@ -225,6 +224,7 @@ public class Game : MonoBehaviour
         cell.flagged = !cell.flagged;
         state[MousePositionOnGameBoard().x, MousePositionOnGameBoard().y] = cell;
         gameBoard.Draw(width, height, state);
+        FlagThemAll();
 
     }
 
@@ -357,6 +357,33 @@ public class Game : MonoBehaviour
     private bool IsValid(int x,int y)
     {
         return x>=0 && x<width && y>=0 && y<height;
+    }
+
+    private void FlagThemAll()
+    {
+        bool cheat = false;
+        int count = 0;
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Cell cell = state[x, y];
+
+                if (cell.type == Cell.Type.Mine && cell.revealed)
+                {
+                    count++;
+                }
+                if (cell.type != Cell.Type.Mine && cell.flagged)
+                {
+                    cheat = true;
+                }
+            }
+        }
+        if (count == mineCount && !cheat)
+        {
+            winLoseText.text = "You won !";
+            gameOver = true;
+        }
     }
 
 }
